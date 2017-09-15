ac51db8ca558a8c27fa1843fa930abc66221d1cd
Max Xu <sixu@cisco.com>
2017-09-15 15:59:23 +0800

    update interface.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -47,11 +47,11 @@
     window.getPhoneticAudioUrl = function (wordContent, audioFile) {
         return ["assets", "words", wordContent.substring(0, 1), wordContent, audioFile].join("/");
     };
-    window.getWordsByCategory = function (categories, category) {
+    window.getItemsByCategoryId = function (categories, categoryId) {
         for (var index in categories) {
-            var item = categories[index];
-            if (item.category == category) {
-                return item.words;
+            var category = categories[index];
+            if (category.id == categoryId) {
+                return category.items;
             }
         }
         return [];
@@ -65,8 +65,8 @@
         'template': '<nav class="navbar navbar-default navbar-fixed-bottom nav-swf"><div class="container"><div class="row"><div class="col-xs-6"><a href="index.html"><img class="img-responsive img-rounded center-block icon" src="assets/images/icon-home.jpg" alt="首页"></a></div><div class="col-xs-6"><a href="index.html"><img class="img-responsive img-rounded center-block icon" src="assets/images/icon-profile.jpg" alt="个人中心"></a></div></div></div></nav>'
     });
     Vue.component('words-category', {
-        'template': '<div class="container"><div class="row"><template v-for="item in items"><div v-on:click="linkTo(item);" class="col-xs-6"><div class="alert alert-success" role="alert"><h4><b>{{ item.category }}</b> <span class="badge">{{ item.words.length }}</span></h4><template v-for="(word, index) in item.words"><template v-if="index< 2"><p>{{ word | toString }}</p></template><template v-else-if="index == 2"><p>.....</p></template></template></div></div></template></div></div>',
-        'props': ['items'],
+        'template': '<div class="container"><div class="row"><template v-for="category in categories"><div v-on:click="linkTo(category);" class="col-xs-6"><div class="alert alert-success" role="alert"><h4><b>{{ category.id }}</b> <span class="badge">{{ category.items.length }}</span></h4><template v-for="(word, index) in category.items"><template v-if="index< 2"><p>{{ word | toString }}</p></template><template v-else-if="index == 2"><p>.....</p></template></template></div></div></template></div></div>',
+        'props': ['categories'],
         'methods': {
             'linkTo': function (item) {
                 if (window.linkTo) {
